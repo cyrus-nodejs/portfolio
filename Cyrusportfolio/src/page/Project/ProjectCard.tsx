628b299e3941e6295a1d24ab2273524eedbfd8ef
baker-allison <aceglobalsystems@gmail.com>
2024-11-19 06:17:30 +0100

    second commit

--- Cyrusportfolio/src/page/Project/ProjectCard.tsx
+++ Cyrusportfolio/src/page/Project/ProjectCard.tsx
@@ -15,7 +15,7 @@ const ProjectCard = ({ imgL,title, field, url }) => {
     <Col  className="Projectframe "    >
       <Image className="pImage" src={imgL}  style={{borderRadius:"5px", margin:""}} fluid />
      
-  <Link to={url}><div className="d-flex bg-danger rounded-2 flex-column middle ">
+  <Link to={url}><div className="d-flex bg-dark rounded-2 flex-column middle ">
   <div className="p-1">{title}</div>
   <div className="p-1">{field}</div>
   <div className="p-1 stack">Click to Visit</div>
